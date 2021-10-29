66e25752e5490950869ffd1d381c69f032e96ebb
Runningtime <Runningtime@163.com>
2021-10-29 11:21:50 +0800

    third commit

--- src/main/com.atguigu.git/HelloGit0625.java
+++ src/main/com.atguigu.git/HelloGit0625.java
@@ -11,5 +11,6 @@ public class HelloGit0625 {
         System.out.println("hello git first");
         System.out.println("hello master");
         System.out.println("hello hot-fix");
+        System.out.println("test1");
     }
 }
---
+++ src/main/com.atguigu.git/Test2.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author: 小河
+ * @date: 2021/10/29
+ * @description:
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("test2");
+    }
+}
