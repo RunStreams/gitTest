61d2b99d8dddd14efb4f2944fdc891fb195f20eb
Runningtime <Runningtime@163.com>
2021-10-29 10:19:15 +0800

    second commit

---
+++ src/main/com.atguigu.git/HelloGit0625.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author: 小河
+ * @date: 2021/10/29
+ * @description:
+ */
+public class HelloGit0625 {
+    public static void main(String[] args) {
+        System.out.println("hello git0625");
+        System.out.println("hello git first");
+    }
+}
