8912f09e6b720d425ac0eaba39b531ff650a452d
Runningtime <Runningtime@163.com>
2021-10-29 10:23:28 +0800

    master commit

--- src/main/com.atguigu.git/HelloGit0625.java
+++ src/main/com.atguigu.git/HelloGit0625.java
@@ -9,5 +9,6 @@ public class HelloGit0625 {
     public static void main(String[] args) {
         System.out.println("hello git0625");
         System.out.println("hello git first");
+        System.out.println("hello master");
     }
 }
